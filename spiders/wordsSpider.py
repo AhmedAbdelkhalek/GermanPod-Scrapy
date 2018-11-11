1b404d0065dd79feb91b6a132d118e4d043033be
AhmedAbdelkhalek <a.kamal_89@yahoo.co.uk>
2018-11-11 12:25:13 +0200

    implement bs4 initial code
    Created to test if boxes are returned correctly.

--- spiders/wordsSpider.py
+++ spiders/wordsSpider.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from bs4 import BeautifulSoup
 
 class WordsSpider(scrapy.Spider):
     name = 'wordsSpider'
@@ -8,4 +8,7 @@ class WordsSpider(scrapy.Spider):
     start_urls = ['https://www.germanpod101.com/german-word-lists/']
 
     def parse(self, response):
-        pass
+        soup = BeautifulSoup(response.body, "html.parser")
+        word_box = soup.find_all("", {"class": "wlv-item__box"})
+        print(response.url)
+             
